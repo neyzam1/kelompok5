b27f04cbfd58648078c192697b97515e714309d9
neyzam1 <150427665+neyzam1@users.noreply.github.com>
2023-11-18 14:46:10 +0800

    Update page.tsx

--- kelompok5/src/app/page.tsx
+++ kelompok5/src/app/page.tsx
@@ -5,10 +5,13 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <div>
+      
       <Head>
         <title>5 Fakta Unik Singa</title>
       </Head>
+      
       <h1>5 Fakta Unik Singa</h1>
+      
       <p>Singa adalah salah satu hewan karnivora terbesar dan terkuat di dunia. Mereka dikenal sebagai raja hutan karena keberanian dan kekuasaan mereka. Namun, tahukah Anda bahwa ada banyak fakta unik tentang singa yang mungkin belum Anda ketahui? Berikut ini adalah lima di antaranya:</p>
       <ol>
         <li><b>Singa adalah satu-satunya anggota keluarga kucing yang memiliki ekor berumbai.</b> Rumbai berfungsi sebagai menyampaikan pesan dari singa, yang berarti seperti “kemarilah”[^1^][1].</li>
@@ -17,8 +20,11 @@ export default function Page() {
         <li><b>Ada singa yang tinggal di hutan.</b> Singa Asia ( Panthera leo persica ) adalah salah satu subspesies singa yang kini hanya tersisa di Gujarat, India. Mereka hidup di Taman Nasional Hutan Gir[^2^][2].</li>
         <li><b>Singa terbesar yang pernah tercatat ialah 690 pound.</b> Singa ini ditemukan di Afrika Selatan pada tahun 1963. Singa berdiri sekitar 1 hingga 1.2 meter. Singa jantan memiliki panjang sekitar 3 meter dan panjang ekornya mencapai 69 hingga 91 cm[^3^][3].</li>
       </ol>
+      
       <p>Itulah lima fakta unik singa yang menarik untuk diketahui. Singa memang hewan yang luar biasa dan patut dihormati. Jika Anda ingin melihat lebih banyak gambar singa, Anda dapat mengunjungi [situs ini](https://www.pinterpandai.com/fakta-singa/).</p>
+      
       <Image src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kompas.com%2Fsains%2Fread%2F2023%2F08%2F07%2F080000723%2Fmengapa-singa-memiliki-surai-%3Fpage%3Dall&psig=AOvVaw0gwYYMAAEEbuxN0QNlFpRH&ust=1700375877401000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDAn9n3zIIDFQAAAAAdAAAAABAE" alt="Gambar singa" width={500} height={300} />
+    
     </div>
   )
 }
